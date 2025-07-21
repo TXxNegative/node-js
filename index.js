044e43bb7ad705be89cb4c0b127a76177af17286
ABHI <physicsevil007@gmail.com>
2025-07-21 12:51:51 +0530

    day_1 of js

---
+++ index.js
@@ -0,0 +1,38 @@
+/*console.log("name: abhijit duuta");
+console.log("age: 21");
+console.log("date of birth: 29-12-2003");
+console.log("university: JIS University");
+console.log("phone number: 9875870115");
+console.log("state: west bengal");
+console.log("father name: anup dutta");
+console.log("mother name: kajal dutta");*/
+
+let a=10
+let b=20
+const sub=b-a
+const mul=a*b
+const div=a/b
+console.log(`subtraction of two numbers are ${sub}, multiplication of two numbers are ${mul}, divsion of the numbers are${div}`);
+
+let base=20
+let height=40
+const aot=0.5*(base*height)
+console.log(`area of triangle is ${aot}`);
+
+const pi=3.14
+let radius=5
+const aoc=pi*(radius**2)
+const poc=2*(pi*radius)
+console.log(`area of circle is ${aoc}`);
+console.log(`perimeter of circle is ${poc}`);
+
+let length=10
+let bredth=20
+const aos=length*bredth
+console.log(`area of square is ${aos}`);
+
+let loc=30
+let boc=40
+let hoc=50
+const aocube=loc*boc*hoc
+console.log(`area of cube is ${aocube}`);
---
+++ obj.js
@@ -0,0 +1,9 @@
+const dict={
+    name:"abhijit dutta",
+    age:21,
+    dob:'29-12-2003',
+    phone_num:9875870115,
+    unmarried:true
+}
+console.log(dict);
+console.log(typeof dict.unmarried);
